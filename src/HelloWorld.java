b96dcd9a609be76544a41bd7b25ce0c1e48a0273
BT_1N3_10 <sample@bths.edu>
2025-11-21 10:16:23 -0500

    added a randomized message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        boolean isFriday = (Math.random() * 7 ) +1 == 6;
+        if (isFriday){
+            System.out.println("Yay it's Friday!");
+        } else {
+            System.out.println("Can't wait for Friday");
+        }
+
+
     }
 }
