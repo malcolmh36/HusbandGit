a1489fcdb26f7ef12a8af1f906f10c46c90ce6b8
BT_1N3_10 <sample@bths.edu>
2025-11-21 10:57:52 -0500

    added another exclamation point to every line that has one

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("I wonder what day today is!");
+        System.out.println("I wonder what day today is!!");
         double weekday = (Math.random() * 7 ) +1;
         boolean isFriday = (Math.random() * 7 ) +1 <= 6;
         if (isFriday){
-            System.out.println("Yay it's Friday!");
+            System.out.println("Yay it's Friday!!");
         } else  if (weekday == 7 || weekday ==1){
-            System.out.println("It's the weekend!");
+            System.out.println("It's the weekend!!");
         }else{
             System.out.println("It's another day...");
         }
--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -1,5 +1,5 @@
 public class HelloWorld2 {
     public static void main(String[] args) {
-        System.out.println("It's a beautiful day today!");
+        System.out.println("It's a beautiful day today!!");
     }
 }
