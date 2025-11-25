b9209c0d13c2e4bd5cf377a9dff5a9f0e1e9c05f
BT_1N3_10 <sample@bths.edu>
2025-11-25 09:42:52 -0500

    Created a class named Git and added the first part of code

---
+++ src/Git.java
@@ -0,0 +1,10 @@
+public class Git {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 5) {
+            System.out.println(counter);
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+    }
+}
