4f2932354706f22119a16c495161e73ca5f73c72
BT_1N3_10 <sample@bths.edu>
2025-11-25 09:45:24 -0500

    tsted all five parts of code

--- src/Git.java
+++ src/Git.java
@@ -1,10 +1,10 @@
 public class Git {
     public static void main(String[] args) {
         int counter = 0;
-        while (counter < 5) {
-            System.out.println(counter);
+        while (counter >= 0) {
             counter++;
         }
         System.out.println("final counter: " + counter);
+
     }
 }
