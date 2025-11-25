35747348b6af3bea7869d6a3e1334d7686551635
Malcolm <malcolmh36@nycstudents.net>
2025-11-25 17:37:31 -0500

    Finished the WhileLoopFun class

--- src/WhileLoopFunRunner.java
+++ src/WhileLoopFunRunner.java
@@ -29,5 +29,18 @@ public class WhileLoopFunRunner {
         System.out.println(fun.maxDoubles(8, 5));
         System.out.println(fun.maxDoubles(10, 456000));
 
+        System.out.println("-- test isPrime --");
+        System.out.println(fun.isPrime(2));
+        System.out.println(fun.isPrime(13));
+        System.out.println(fun.isPrime(773));
+        System.out.println(fun.isPrime(9857));
+        System.out.println(fun.isPrime(101879));
+        System.out.println(fun.isPrime(1));
+        System.out.println(fun.isPrime(14));
+        System.out.println(fun.isPrime(30));
+        System.out.println(fun.isPrime(771));
+        System.out.println(fun.isPrime(93243));
+
+
     }
 }
